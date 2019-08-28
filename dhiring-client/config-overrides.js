f86aac9188486966a5f61e314e790fb43463d43d
zhengchengc <zhengchengc@student.unimelb.edu.au>
2019-08-28 16:00:54 +1000

    initialize the project, decide the technical selection

---
+++ dhiring-client/config-overrides.js
@@ -0,0 +1,25 @@
+/**
+ * File: /config-overrides.js                                                 * 
+ * Project: dhiring-client                                                    * 
+ * Created Date: Wednesday, August 28th 2019, 2:30:27 pm                      * 
+ * Author: zhengchengc <chen.zhengcheng@mailbox.org>                          * 
+ * -----                                                                      * 
+ * Last Modified: Wednesday August 28th 2019 2:30:27 pm                       * 
+ * Modified By: zhengchengc at <chen.zhengcheng@mailbox.org>                  * 
+ * -----                                                                      * 
+ * Copyright (c) 2019 Olichid                                                 * 
+ * ------------------------------------                                       * 
+ * Live long and prosper!                                                     * 
+ */
+
+const { override, fixBabelImports, addLessLoader } = require('customize-cra');
+
+module.exports = override(
+    fixBabelImports("import", {
+      libraryName: "antd", libraryDirectory: "es", style: true // change importing css to less
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: { "@primary-color": "#1DA57A" }
+    })
+  );
---
+++ dhiring-client/src/index.js
@@ -0,0 +1,19 @@
+/**
+ * File: /src/index.js                                                        * 
+ * Project: dhiring-client                                                    * 
+ * Created Date: Wednesday, August 28th 2019, 2:16:41 pm                      * 
+ * Author: zhengchengc <chen.zhengcheng@mailbox.org>                          * 
+ * -----                                                                      * 
+ * Last Modified: Wednesday August 28th 2019 2:16:41 pm                       * 
+ * Modified By: zhengchengc at <chen.zhengcheng@mailbox.org>                  * 
+ * -----                                                                      * 
+ * Copyright (c) 2019 Olichid                                                 * 
+ * ------------------------------------                                       * 
+ * Live long and prosper!                                                     * 
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd-mobile';
+
+ReactDOM.render(<Button type="primary">Test</Button>, document.getElementById('root'));

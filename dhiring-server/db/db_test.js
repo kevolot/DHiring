41ddbb035a011e9416f50ac35547dac4a1869509
zhengchengc <zhengchengc@student.unimelb.edu.au>
2019-08-30 09:20:17 +1000

    introduce mongodb and mongoose

---
+++ dhiring-server/db/db_test.js
@@ -0,0 +1,75 @@
+/**
+ * Description.
+ *
+ * @author zhengchengc (chen.zhengcheng@mailbox.org)
+ * @since 01:01 30 Aug 2019
+ */
+
+/**
+ * Use mongoose to manipulate mongodb
+ * 1. Connect to database
+ *      1.1 Introduce mongoose
+ *      1.2 Connect database
+ *      1.3 acquire object
+ *      1.4 binding connection to listen
+ * 2. get model for specific cluster
+ *      2.1 Schema, describe structure of document
+ *      2.2 Define Model
+ * 3. CRUD actions
+ *      3.1 Using save() to add data
+ *      3.2 Using find()/findOne() to find one or more data entries
+ *      3.3 Using findByIdAndUpdate() to update specific data
+ *      3.4 Using remove() to delete specific data
+ */
+
+const md5 = require('blueimp-md5');
+
+// 1. Connect to database
+// 1.1 Introduce mongoose
+const mongoose = require('mongoose');
+
+// 1.2 Connect database
+mongoose.connect('mongodb://localhost:27017/dhiring_test', {useNewUrlParser: true})
+    .catch(e => {
+        console.error('Connection error', e.message);
+    });
+
+// 1.3 acquire object
+const conn = mongoose.connection;
+
+// 1.4 Binding connection to listen
+conn.on('error', console.error.bind(console, 'connection error:'));
+// 2. Get model for specific cluster
+// 2.1 Schema
+const userSchema = mongoose.Schema({
+    username: {type: String, required: true},
+    password: {type: String, required: true},
+    type: {type: String, required: true}
+});
+
+// 2.2 Define Model
+const UserModel = mongoose.model('user', userSchema);
+
+// 3. CRUD
+// 3.1 Using save() to add data
+function testSave() {
+    // user data object
+    const user = {
+        username: 'markz',
+        password: md5('1234'),
+        type: 'hunter'
+    };
+    const userModel = new UserModel(user);
+    // save to database
+    userModel.save(function (err, user) {
+        console.log('save', err, user);
+    });
+}
+
+// 3.2 Using find()/findOne() to find one or more data entries
+function testFind() {
+    // find multiple
+    UserModel.find(function (err, users) {
+        console.log('find()', err, users);
+    });
+}

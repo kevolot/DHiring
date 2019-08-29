9496094a16e108110ad2e839584fc3df034d1f8d
zhengchengc <zhengchengc@student.unimelb.edu.au>
2019-08-29 11:26:44 +1000

    collect register data

--- dhiring-client/src/containers/register/register.jsx
+++ dhiring-client/src/containers/register/register.jsx
@@ -14,28 +14,54 @@
 import React, { Component } from 'react';
 import { NavBar,WingBlank, List, InputItem, WhiteSpace, Radio, Button } from 'antd-mobile';
 import Logo from '../../components/logo/logo';
+import { register } from '../../redux/actions';
 
 const ListItem = List.Item;
 
 
 export default class Register extends Component {
+    state = {
+        username:'',
+        password:'',
+        confirmp:'',
+        type:'hunter', //Hunter or Boss
+    };
+
+    register = () => {
+        console.log(this.state);
+    };
+
+    // process change of input date, update relative state
+    handleChange = (name, val) => {
+
+        // update state
+        this.setState({
+            [name]: val
+        })
+    };
+
+    toLogin = () => {
+        this.props.history.replace('/login');
+    };
+
     render() {
+        const {type} = this.state;
         return (
             <div>
                 <NavBar>DHiring</NavBar>
                 <Logo />
                 <WingBlank>
                     <List>
-                        <InputItem>Username:</InputItem>
-                        <InputItem type="password">Password:</InputItem>
-                        <InputItem type="password">Confirm:</InputItem>
+                        <InputItem onChange={val => {this.handleChange('username', val)}}>Username:</InputItem>
+                        <InputItem type="password" onChange={val => {this.handleChange('password', val)}}>Password:</InputItem>
+                        <InputItem type="password" onChange={val => {this.handleChange('confirmp', val)}}>Confirm:</InputItem>
                         <ListItem>
                             <span>User Type:</span>
-                            <Radio>Hunter</Radio>
-                            <Radio>Boss</Radio>
+                            <Radio chekced={type === 'hunter'} onChange={() => this.handleChange('type', 'hunter')}>Hunter</Radio>
+                            <Radio chekced={type === 'boss'} onClick={() => this.handleChange('type', 'boss')}>Boss</Radio>
                         </ListItem>
-                        <Button type="primary">Register</Button>
-                        <Button>Login</Button>
+                        <Button type="primary" onClick={this.register}>Register</Button>
+                        <Button onClick={this.toLogin}>Login</Button>
                     </List>
                 </WingBlank>
             </div>

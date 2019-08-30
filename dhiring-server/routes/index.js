4ca6d082d4b342ccf6e566f86ba826ce44558b47
zhengchengc <zhengchengc@student.unimelb.edu.au>
2019-08-30 10:38:54 +1000

    finish login router

--- dhiring-server/routes/index.js
+++ dhiring-server/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 const md5 = require('blueimp-md5');
 const { UserModel } = require('../db/models');
+const filter = {password: 0};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -23,7 +24,7 @@ router.post('/register', function (req, res) {
       // save the user
       new UserModel({ username, password: md5(password), type }).save(function (err, user) {
         // generate a cookie, save in browser
-        res.cookie('userid', user._id, {maxAge: 1000*60*60^24*7});
+        res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7});
         // return json include user data
         const data = { username, type, _id: user._id }; // DON'T INCLUDE PASSWORD IN RETURN MESSAGE
         res.send({code: 0, data });
@@ -32,5 +33,17 @@ router.post('/register', function (req, res) {
   })
 });
 // login router
+router.post('/login', function (req, res) {
+  const { username, password } = req.body;
+
+  UserModel.findOne({username, password: md5(password)}, filter, function (err, user) {
+    if (user) {
+      res.cookie('userid', user._id, {maxAge: 1000*60*60*24*7});
+      res.send({code: 0, data: user});
+    } else {
+      res.send({code: 1, msg: 'username or password are not correct!'});
+    }
+  })
+})
 
 module.exports = router;
